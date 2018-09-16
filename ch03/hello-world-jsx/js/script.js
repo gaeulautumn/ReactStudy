06a091f6633b2d52e1d8565eb266678e7ef6568f
gaeulautumn <gaeulautumnlee@gmail.com>
2018-09-16 23:37:36 +0900

    props in JSX

--- ch03/hello-world-jsx/js/script.js
+++ ch03/hello-world-jsx/js/script.js
@@ -1,18 +1,28 @@
-
-let helloWorldElement = React.createElement(
-  'h1',
-  null,
-  'Hello World!'
-);
-
 class HelloWorld extends React.Component {
   render() {
     return React.createElement(
-      'div',
-      null,
-      helloWorldElement
+      'h1',
+      this.props,
+      'Hello ',
+      this.props.frameworkName,
+      ' world!!!'
     );
   }
 }
 
-ReactDOM.render(React.createElement(HelloWorld, null), document.getElementById('content'));
+ReactDOM.render(React.createElement(
+  'div',
+  null,
+  React.createElement(HelloWorld, {
+    id: 'ember',
+    frameworkName: 'Ember.js',
+    title: 'A framework for creating ambitious web applications.' }),
+  React.createElement(HelloWorld, {
+    id: 'backbone',
+    frameworkName: 'Backbone.js',
+    title: 'Backbone.js gives structure to web applications...' }),
+  React.createElement(HelloWorld, {
+    id: 'angular',
+    frameworkName: 'Angular.js',
+    title: 'Superheroic JavaScript MVW Framework' })
+), document.getElementById('content'));
--- ch03/hello-world-jsx/jsx/script.jsx
+++ ch03/hello-world-jsx/jsx/script.jsx
@@ -1,15 +1,25 @@
-
-let helloWorldElement = <h1>Hello World!</h1>
-
 class HelloWorld extends React.Component {
   render() {
-    return <div>
-      {helloWorldElement}
-    </div>
+    return <h1 {...this.props}>
+        Hello {this.props.frameworkName} world!!!
+      </h1>
   }
 }
 
 ReactDOM.render(
-    <HelloWorld/>,
-    document.getElementById('content')
-  )
+  <div>
+    <HelloWorld
+      id='ember'
+      frameworkName='Ember.js'
+      title='A framework for creating ambitious web applications.'/>
+    <HelloWorld
+      id='backbone'
+      frameworkName='Backbone.js'
+      title='Backbone.js gives structure to web applications...'/>
+    <HelloWorld
+      id='angular'
+      frameworkName='Angular.js'
+      title='Superheroic JavaScript MVW Framework'/>
+  </div>,
+  document.getElementById('content')
+)
