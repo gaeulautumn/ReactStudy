c7b6ed43aa48a4160623198a69e32722e6505bb2
gaeulautumn <gaeulautumnlee@gmail.com>
2018-11-12 23:24:40 +0900

    nested routing

--- router_practice/src/App.js
+++ router_practice/src/App.js
@@ -2,15 +2,55 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AppRouter from './AppRouter';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <AppRouter/>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Header />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} />
+    </div>
+  </Router>
+);
+
+// match : params, path
+const Home = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+const Topic = ({ match }) => <h3>Requested param : {match.params.id}</h3>
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.path}/:id`} component={Topic} />
+    <Route
+      exact path={match.path} render={() => <h3>Please select a topic.</h3>}
+    />
+  </div>
+);
+
+const Header = () => (
+  <ul>
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+    <li>
+      <Link to="/about">About</Link>
+    </li>
+    <li>
+      <Link to="/topics">Topics</Link>
+    </li>
+  </ul>
+);
 
 export default App;
